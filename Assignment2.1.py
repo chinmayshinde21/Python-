6a2857dbaa6302d1331401cd1c6fa06d57ec7fd4
chinmayshinde21 <shindechinmay1234@gmail.com>
2026-06-12 16:55:08 +0530

    Assignment2.1

---
+++ Assignment2.1.py
@@ -0,0 +1,12 @@
+X = 11
+Y = 11
+
+print(X)           
+print(type(X))
+print(id(X))
+print(id(Y))
+
+a = int(input("Enter a number: "))           # taking input from user and converting it to integer
+print(a)
+print(type(a))
+print(id(a))
