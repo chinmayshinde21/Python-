0db58f3e78683cbf00d011656cb38c7b653e99cf
chinmayshinde21 <shindechinmay1234@gmail.com>
2026-06-12 17:28:40 +0530

    Assignment2.9

---
+++ Assignment2.9.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")           # taking input from user and storing it in variable name
+age = int(input("Enter your age: "))            # taking input from user and converting it to integer
+
+
+print(f"Hello, {name}! You will turn {age+1} next year.")   # using f-string to format the output
